d4abf79702df6e9d3a88c8a403b704942e602fec
starkwang <381152119@qq.com>
2015-08-08 23:44:17 +0800

    add matrix

---
+++ js/image.js
@@ -0,0 +1,87 @@
+(function() {
+
+    function getTwoDimenArray(x, y) {
+        var matrix = new Array(x);
+        for (var i = 0; i < x; i++) {
+            matrix[i] = new Array(y);
+        }
+        return matrix;
+    }
+
+
+    function matrixFactory(R, G, B, A) {
+        this.R = R;
+        this.G = G;
+        this.B = B;
+        this.A = A;
+    }
+
+
+    function toMatrix(pixelData, imageWidth) {
+        var matrix = getTwoDimenArray(imageWidth, pixelData.length / 4 / imageWidth);
+
+        for (var i = 0, length = pixelData.length; i < length; i += 4) {
+            var y = Math.floor(i / (4 * imageWidth));
+            var x = (i / 4) % imageWidth;
+            matrix[x][y] = new matrixFactory(pixelData[i], pixelData[i + 1], pixelData[i + 2], pixelData[i + 3]);
+        }
+
+        return matrix;
+    }
+
+    function calculateByMatrix(dataMatrix, coreMatrix) {
+        if (coreMatrix.length % 2 == 0) {
+            alert('核矩阵不正确！');
+            return;
+        }
+        var newMatrix = getTwoDimenArray(dataMatrix.length, dataMatrix[0].length);
+        var center = (coreMatrix.length - 1) / 2;
+        var coreSize = coreMatrix.length;
+
+        var round = Math.round;
+        for (var i = 0, ilength = dataMatrix.length; i < ilength; i++) {
+            for (var j = 0, jlength = dataMatrix[i].length; j < jlength; j++) {
+                var R, G, B;
+                var rTotal = 0;
+                var gTotal = 0;
+                var bTotal = 0;
+                var average = 0;
+                for (var x = i - center, xlength = i + center, countx = 0; x <= xlength; x++) {
+                    for (var y = j - center, ylength = j + center, county = 0; y <= ylength; y++) {
+
+                        if (x < 0 || y < 0) {
+                            continue;
+                        }
+                        if (x >= dataMatrix.length || y >= dataMatrix[0].length) {
+                            continue;
+                        }
+
+                        try {
+                            rTotal += dataMatrix[x][y].R * coreMatrix[county][countx];
+                        } catch (e) {
+                            console.log(x, y);
+                            window.dataMatrix = dataMatrix;
+                        }
+
+                        gTotal += dataMatrix[x][y].G * coreMatrix[county][countx];
+                        bTotal += dataMatrix[x][y].B * coreMatrix[county][countx];
+                        average += coreMatrix[county][countx];
+                        county++;
+                    }
+                    countx++;
+                }
+                R = round(rTotal / average);
+                G = round(gTotal / average);
+                B = round(bTotal / average);
+                newMatrix[i][j] = new matrixFactory(R, G, B);
+            }
+        }
+
+        return newMatrix;
+    }
+
+    window.Simage = {
+        toMatrix: toMatrix,
+        calculateByMatrix: calculateByMatrix
+    }
+})()
--- js/index.js
+++ js/index.js
@@ -1,48 +1,63 @@
- function draw(img, width, height) {
-   var _self = this;
-   var canvas = document.getElementById("canvas");
-   var context = canvas.getContext("2d");
-   context.shadowBlur = 20;
-   context.shadowColor = "#DDDDDD";
-   context.drawImage(img, 0, 0, width, height);
-   var imageData = context.getImageData(0, 0, 1080, 900);
-   var pixel = imageData.data;
-   for (var i = 0, length = pixel.length; i < length; i += 4) {
-     //pixel[i + 2] = pixel[i + 1] = pixel[i] = (pixel[i] * 19595 + pixel[i + 1] * 38469 + pixel[i + 2] * 7472) >> 16;
-   }
-
-   canvas = $("#canvas");
-   canvas.click(function(e) {
-     var canvasOffset = canvas.offset();
-     var canvasX = Math.floor(e.pageX - canvasOffset.left);
-     var canvasY = Math.floor(e.pageY - canvasOffset.top);
-     // 获取该点像素的数据
-     var imageData = context.getImageData(canvasX, canvasY, 1, 1);
-     // 获取该点像素数据
-     var pixel = imageData.data;
-     var pixelColor = "rgba(" + pixel[0] + "," + pixel[1] + "," + pixel[2] + "," + pixel[3] + ")";
-     $("body").css("backgroundColor", pixelColor);
-     $("#currentColor").html("当前背景颜色为:" + pixelColor);
-   });
- }
- $(document).ready(function() {
-   var img = new Image();
-   img.src = "../img/pic.jpg";
-   $(img).load(function() {
-     draw(img, 1080, 900);
-   });
+function draw(img, width, height) {
+    var _self = this;
+    var canvas = document.getElementById("canvas");
+    var context = canvas.getContext("2d");
+    context.shadowBlur = 20;
+    context.shadowColor = "#DDDDDD";
+    context.drawImage(img, 0, 0, width, height);
+    var imageData = context.getImageData(0, 0, 1080, 900);
+    var pixel = imageData.data;
+    var matrix = Simage.toMatrix(pixel, 1080);
 
 
 
 
-   $('.change').click(function() {
-     var context = $('#canvas')[0].getContext('2d');
-     var imageData = context.getImageData(0, 0, 1080, 900);
-     var pixel = imageData.data;
-     for (var i = 0, length = pixel.length; i < length; i += 4) {
-       pixel[i + 2] = pixel[i + 1] = pixel[i] = (pixel[i] * 2 + pixel[i + 1] * 5 + pixel[i + 2] * 1) >> 3;
-     }
-     imageData.data = pixel;
-     context.putImageData(imageData, 0, 0);
-   })
- });
+    var coreMatrix = [
+        [1, 2, 1],
+        [2, 4, 2],
+        [1, 2, 1]
+    ];
+    var newMatrix = Simage.calculateByMatrix(matrix, coreMatrix);
+    console.log(newMatrix);
+    for (var i = 0, length = pixel.length; i < length; i += 4) {
+        //pixel[i + 2] = pixel[i + 1] = pixel[i] = (pixel[i] * 19595 + pixel[i + 1] * 38469 + pixel[i + 2] * 7472) >> 16;
+    }
+
+    canvas = $("#canvas");
+    canvas.click(function(e) {
+        var canvasOffset = canvas.offset();
+        var canvasX = Math.floor(e.pageX - canvasOffset.left);
+        var canvasY = Math.floor(e.pageY - canvasOffset.top);
+        // 获取该点像素的数据
+        var imageData = context.getImageData(canvasX, canvasY, 1, 1);
+        // 获取该点像素数据
+        var pixel = imageData.data;
+
+        var pixelColor = "rgba(" + pixel[0] + "," + pixel[1] + "," + pixel[2] + "," + pixel[3] + ")";
+        $("body").css("backgroundColor", pixelColor);
+        console.log("当前背景颜色为:" + matrix[canvasX][canvasY].R + ',' + matrix[canvasX][canvasY].G + ',' + matrix[canvasX][canvasY].B);
+        console.log("new背景颜色为:" + newMatrix[canvasX][canvasY].R + ',' + newMatrix[canvasX][canvasY].G + ',' + newMatrix[canvasX][canvasY].B);
+    });
+}
+$(document).ready(function() {
+    var img = new Image();
+    img.src = "../img/pic.jpg";
+    $(img).load(function() {
+        draw(img, 1080, 900);
+    });
+
+
+
+
+    $('.change').click(function() {
+        var context = $('#canvas')[0].getContext('2d');
+        var imageData = context.getImageData(0, 0, 1080, 900);
+        var pixel = imageData.data;
+        // for (var i = 0, length = pixel.length; i < length; i += 4) {
+        //     pixel[i + 2] = pixel[i + 1] = pixel[i] = (pixel[i] * 2 + pixel[i + 1] * 5 + pixel[i + 2] * 1) >> 3;
+        // }
+
+        //imageData.data = pixel;
+        context.putImageData(imageData, 0, 0);
+    })
+});
