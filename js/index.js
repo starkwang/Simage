b9837caf08d1fa20e7a147d092debc02428ca419
starkwang <381152119@qq.com>
2015-08-08 18:08:18 +0800

    a

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
- function draw(img) {
+ function draw(img, width, height) {
    var _self = this;
    var canvas = document.getElementById("canvas");
    var context = canvas.getContext("2d");
    context.shadowBlur = 20;
    context.shadowColor = "#DDDDDD";
-   context.drawImage(img, 0, 0);
-   var imageData = context.getImageData(0, 0, 10, 1);
+   context.drawImage(img, 0, 0, width, height);
+   var imageData = context.getImageData(0, 0, 1080, 900);
    var pixel = imageData.data;
-   console.log(pixel);
    for (var i = 0, length = pixel.length; i < length; i += 4) {
-     console.log(i + ":rgb(" + pixel[i] + "," + pixel[i + 1] + "," + pixel[i + 2] + ")");
+     //pixel[i + 2] = pixel[i + 1] = pixel[i] = (pixel[i] * 19595 + pixel[i + 1] * 38469 + pixel[i + 2] * 7472) >> 16;
    }
+
    canvas = $("#canvas");
    canvas.click(function(e) {
      var canvasOffset = canvas.offset();
@@ -29,6 +29,20 @@
    var img = new Image();
    img.src = "../img/pic.jpg";
    $(img).load(function() {
-     draw(img);
+     draw(img, 1080, 900);
    });
+
+
+
+
+   $('.change').click(function() {
+     var context = $('#canvas')[0].getContext('2d');
+     var imageData = context.getImageData(0, 0, 1080, 900);
+     var pixel = imageData.data;
+     for (var i = 0, length = pixel.length; i < length; i += 4) {
+       pixel[i + 2] = pixel[i + 1] = pixel[i] = (pixel[i] * 2 + pixel[i + 1] * 5 + pixel[i + 2] * 1) >> 3;
+     }
+     imageData.data = pixel;
+     context.putImageData(imageData, 0, 0);
+   })
  });
